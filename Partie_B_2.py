9ba2c31cea5c0c878fdffe720a633f8312e3e4d6
Félix Girard <140023087+TheSweetCoder03@users.noreply.github.com>
2026-03-25 15:10:30 -0400

    Ajout calcul de la température. Ajout variable température stagnation, vitesse axiale et cp

--- Partie_B_2.py
+++ Partie_B_2.py
@@ -28,15 +28,23 @@ def etape_2():
     rt_1 = 2
     rt_2 = 2
     rt_3 = 2
-    f_t_m = 0.5 # facteur de travail moyen
+    f_t_m = 0.63 # facteur de travail moyen
+    Va_1 = 1
+    Va_2 = 1
+    Va_3 = 1
+    To_1 = 1
+    To_2 = 1
+    To_3 = 1
+    cp = 1
+
 
     #Hypothèse de Free Vortex
     n = -1
 
     #Trouver les constantes à chaque station
-    stat_1 = [Vu_1, rm_1, Pm_1, alpham_1, pm_1, rr_1, rt_1]
-    stat_2 = [Vu_2, rm_2, Pm_2, alpham_2, pm_2, rr_2, rt_2]
-    stat_3 = [Vu_3, rm_3, Pm_3, alpham_3, pm_3, rr_3, rt_3]
+    stat_1 = [Vu_1, rm_1, Pm_1, alpham_1, pm_1, rr_1, rt_1, Va_1, To_1]
+    stat_2 = [Vu_2, rm_2, Pm_2, alpham_2, pm_2, rr_2, rt_2, Va_2, To_2]
+    stat_3 = [Vu_3, rm_3, Pm_3, alpham_3, pm_3, rr_3, rt_3, Va_3, To_3]
     
     for i in [stat_1, stat_2, stat_3] :
         k1 = i[0] * i[1]
@@ -47,32 +55,41 @@ def etape_2():
     #Calculer variables le long du rayon
     for j in [stat_1, stat_2, stat_3]:
         r = np.linspace(j[5], j[6], 100)
-        f_t = 1 - j[8] / r**2
-        Vu = j[7] / r
-        P = (((j[7]**2)*j[4])/2) * (1/j[1]**2 - 1/r**2) + j[2]
-        alpha = np.arctan(j[9] / r)
+        f_t = 1 - j[10] / r**2
+        Vu = j[9] / r
+        V = np.sqrt(j[10]**2 + Vu**2)
+        T = j[8] - V**2 / (2*cp)
+        P = (((j[9]**2)*j[4])/2) * (1/j[1]**2 - 1/r**2) + j[2]
+        alpha = np.arctan(j[11] / r)
 
-        fig, axes = plt.subplots(2,2, figsize=(10,8))
+        #Faire graphiques
+
+        fig, axes = plt.subplots(2,3, figsize=(10,8))
 
         axes[0, 0].plot(r, f_t)
-        axes[0, 0].set_xlabel("r ")
+        axes[0, 0].set_xlabel("r (m)")
         axes[0, 0].set_ylabel("Λ")
         axes[0, 0].set_title("Λ  vs r")
 
         axes[0, 1].plot(r, Vu)
-        axes[0, 0].set_xlabel("r ")
+        axes[0, 1].set_xlabel("r (m)")
         axes[0, 1].set_ylabel("Vu")
         axes[0, 1].set_title("Vu vs r")
 
         axes[1, 0].plot(r, P)
-        axes[0, 0].set_xlabel("r ")
+        axes[1, 0].set_xlabel("r (m)")
         axes[1, 0].set_ylabel("P")
         axes[1, 0].set_title("P vs r")
 
         axes[1, 1].plot(r, alpha)
-        axes[0, 0].set_xlabel("r ")
+        axes[1, 1].set_xlabel("r (m)")
         axes[1, 1].set_ylabel("Vrillage")
         axes[1, 1].set_title("Vrillage vs r")
+
+        axes[1, 2].plot(r, T)
+        axes[1, 2].set_xlabel("r (m)")
+        axes[1, 2].set_ylabel("T")
+        axes[1, 2].set_title("Température vs r")
 
         plt.tight_layout()
         plt.show()
