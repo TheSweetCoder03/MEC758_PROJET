42f2d55d70cb38713ae284139cd16761a7eccace
Félix Girard <140023087+TheSweetCoder03@users.noreply.github.com>
2026-04-13 16:12:17 -0400

    Les géométries de la turbine se recalculait avec le nouveau rpm. Donc on a modifier Partie_B pour que ca soit fixe quand Partie_C lappel et sinon quelle calcul.

--- Partie_B.py
+++ Partie_B.py
@@ -34,7 +34,7 @@ resultats_radiaux = {}
 def deg2rad(angle):
     return angle * np.pi / 180.0
 
-def etape_1(donnees_hpt, racine_constante, tolerance=1e-6):
+def etape_1(donnees_hpt, racine_constante, tolerance=1e-6, r_root3_fixe=None):
     print(f"\nÉTAPES 1: Géométrie et Triangles")
     print(f"Stratégie de veine : {'Racine Constante' if racine_constante else 'Bout (Tip) Constant'}")
     
@@ -70,7 +70,7 @@ def etape_1(donnees_hpt, racine_constante, tolerance=1e-6):
     Vu3 = V3 * np.sin(deg2rad(contraintes['alpha_3']))
 
     A3 = m_dot / (rho3 * Va3)
-    r_root3 = U_root_cible / omega
+    r_root3 = r_root3_fixe if r_root3_fixe is not None else U_root_cible / omega
     r_tip3 = np.sqrt((A3 / np.pi) + r_root3**2)
     r_m3 = (r_root3 + r_tip3) / 2.0
     U3 = omega * r_m3
--- Partie_C.py
+++ Partie_C.py
@@ -4,14 +4,16 @@ import Partie_B
 
 def partie_c():
     donnees_hpt = Partie_A.export_donnees_hpt()
-    
-    # Réduction de 20% du RPM
-    rpm_design = donnees_hpt['rpmhpc']
-    rpm_hors_conception = rpm_design * 0.80
-    donnees_hpt_hc = {**donnees_hpt, 'rpmhpc': rpm_hors_conception}
-    
-    # Recalculer les triangles de vitesses avec le nouveau RPM
-    Partie_B.etape_1(donnees_hpt_hc, racine_constante=False)
 
-    #Tracer les triangles de vitesses
+    # Récupérer la géométrie fixe
+    r_root3_conception = Partie_B.donnees['r_root'][3]
+
+    # Réduit de 20% le RPM de la HPT
+    rpm_design = donnees_hpt['rpmhpc']
+    donnees_hpt_hc = {**donnees_hpt, 'rpmhpc': rpm_design * 0.80}
+
+    # Recalculer les triangles avec géométrie figée
+    Partie_B.etape_1(donnees_hpt_hc, racine_constante=False, r_root3_fixe=r_root3_conception)
+
+    # Tracer les triangles de vitesses
     Partie_B.tracer_triangles_vitesses()
